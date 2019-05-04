a2b12077f07f96f86d110f51bd60121a078eae0a
hang <1142469709@qq.com>
2019-05-04 21:57:27 +0800

    dev全删

--- dev.js
+++
@@ -1,9 +0,0 @@
-console.log('dev');
-console.log('dev第二次');
-console.log('dev第3次');
-console.log('dev往dev上提交1');
-console.log('dev1往dev上提交1');
-console.log('dev往dev上提交2');
-console.log('dev1往dev上提交10');
-console.log('dev往dev上提交11');
-console.log('dev1往dev上提交12');
