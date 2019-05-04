9e936f3078106fb0cf93f9858ecf3bda3fb97fbe
hang <1142469709@qq.com>
2019-05-04 22:01:38 +0800

    git fetch git rebase避免多条日志问题

--- b.js
+++
@@ -1,8 +0,0 @@
-console.log('v1.0创建的第一个文件');
-console.log('v1.0修改的第一个文件');
-console.log('v1.0修改的第二个文件');
-console.log('v1.0修改的第三个文件');
-console.log('v1.0修改的第四个文件');
-console.log('v1.0修改的第五个文件');
-console.log('v1.0修改的第6个文件');
-console.log('v1.0修改的第7个文件');
